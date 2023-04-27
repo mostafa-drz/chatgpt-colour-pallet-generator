bcb9a81a048fbec92e298495e6b2fb3391eeed2d
Mostafa Darehzereshki <mostafa69d@gmail.com>
2023-04-27 19:48:11 -0400

    adds a better styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { SearchInput } from '@/components/SearchInput'
 import { FormEvent, useState } from 'react'
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,18 +33,21 @@ export default function Home() {
     }
   }
   return (
-    <main style={{height:"100vh"}}>
-      <div style={{height:"100%",padding:"10%"}}>
+    <main className='h-screen'>
+      <div className='h-full p-10'>
       <form onSubmit={handleSubmit}>
       <SearchInput value={query} onChange={handleQueryChange}/>
       <button type='submit'>Search</button>
       </form>
-      <div style={{display:"flex",width:"100vw",justifyContent:"space-between",height:"100%"}}>
+      <div className='flex w-screen flex-col h-full flex-wrap'>
         {
           results.map((result,index) => { 
             return(
-              <div style={{backgroundColor:result,height:'100%',flex:1}}  key={index+result}>
-                <p style={{color:"#fff"}}>{result}</p>
+              <div style={{backgroundColor:result}} className='w-screen  rounded-md border-white border-2 flex-1 mt-2 relative'  key={index+result}>
+                <div className='absolute top-0 left-0 bg-gray-500 w-40 h-10 opacity-50 z-0'/>
+                <CopyToClipboard text={result}>
+                  <button style={{color:"#fff"}} className=' absolute top-0 left-0 z-10 rounded-md text-center p-2'>{result}</button>
+                  </CopyToClipboard>
               </div>
             )
         })}
